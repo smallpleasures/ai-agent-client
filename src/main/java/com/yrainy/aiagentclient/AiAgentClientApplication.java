39818231bfbef0e6e0ca40c8c8e9ae7258e5b72c
zhanglun <yrainy2021@163.com>
2026-09-18 08:54:43 +0800

    EnableSpringUtil

--- src/main/java/com/yrainy/aiagentclient/AiAgentClientApplication.java
+++ src/main/java/com/yrainy/aiagentclient/AiAgentClientApplication.java
@@ -1,9 +1,11 @@
 package com.yrainy.aiagentclient;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSpringUtil
 public class AiAgentClientApplication {
 
     public static void main(String[] args) {
